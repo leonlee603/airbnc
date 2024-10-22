1a6ca0d69f430ce4747fa886b2163c24f31bd890
Leon <leepingho603@gmail.com>
2024-10-22 17:58:32 +0800

    Update - Import some shadcn/ui components and custom components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import "@/app/globals.css";
+import Navbar from "@/components/navbar/Navbar";
 
 export const metadata: Metadata = {
   title: "Airbnc",
-  description: "From cozy cottages to elegant penthouses, Hosts are happy to share their places. Whether its a work trip, weekend getaway, family vacation, or a longer stay, there are millions of amazing places to visit.",
+  description:
+    "From cozy cottages to elegant penthouses, Hosts are happy to share their places. Whether its a work trip, weekend getaway, family vacation, or a longer stay, there are millions of amazing places to visit.",
 };
 
 export default function RootLayout({
@@ -13,10 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={``}
-      >
-        {children}
+      <body className={``}>
+        <Navbar />
+        <main className="container py-10">{children}</main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
+    <div>
       <h1 className="text-3xl">HomePage</h1>
+      <Button variant="outline" size="lg" className="capitalize m-8">Click me</Button>
+    </div>
   );
 }
