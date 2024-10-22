517ed984d448d89b6244ebd08f361c672fbaaba3
Leon <leepingho603@gmail.com>
2024-10-22 18:28:12 +0800

    Update - Navbar components

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,5 +1,13 @@
+import { VscVerifiedFilled } from "react-icons/vsc";
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+
 export default function Logo() {
   return (
-    <div>Logo</div>
+    <Button size='icon' asChild>
+      <Link href='/'>
+        <VscVerifiedFilled className='w-6 h-6' />
+      </Link>
+    </Button>
   )
 }
