df4f63703621fabc8b7486392965f121891b3157
Leon <leepingho603@gmail.com>
2024-10-23 15:53:28 +0800

    Update - Add Clerk to project
    install Clerk, add clerk provider and middleware for route protection

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import "@/app/globals.css";
 import Navbar from "@/components/navbar/Navbar";
-import Providers from "./providers";
+import Providers from "@/app/providers";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Airbnc",
@@ -15,13 +16,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={``}>
-        <Providers>
-          <Navbar />
-          <main className="container py-10">{children}</main>
-        </Providers>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={``}>
+          <Providers>
+            <Navbar />
+            <main className="container py-10">{children}</main>
+          </Providers>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
