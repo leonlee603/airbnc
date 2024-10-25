b8fd9f55498db85f61eb7bc664a54d90312ae5b8
Leon <leepingho603@gmail.com>
2024-10-25 12:38:51 +0800

    Fix - createProfile action

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,34 +1,44 @@
 "use server";
 
 import { profileSchema } from "./schemas";
-import db from './db';
-import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
-import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation';
+import db from "./db";
+import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
-export async function createProfileAction (prevState: any, formData: FormData) {
+export async function createProfileAction(prevState: any, formData: FormData) {
   try {
     // get user info from clerk
     const user = await currentUser();
-    if (!user) throw new Error('Please login to create a profile');
+    if (!user) throw new Error("Please login to create a profile");
 
-    console.log(user);
     // apply validation
     const rawData = Object.fromEntries(formData);
     const validatedFields = profileSchema.parse(rawData);
 
+    // create user profile to database
     await db.profile.create({
       data: {
         clerkId: user.id,
         email: user.emailAddresses[0].emailAddress,
-        profileImage: user.imageUrl ?? '',
+        profileImage: user.imageUrl ?? "",
         ...validatedFields,
-      }
-    })
+      },
+    });
 
-    return { message: 'Profile Created' };
-  } catch (error:any) {
-    const errMsg = error.errors[0]?.message ? error.errors[0].message : "there was an error...";
-    return { message: `${errMsg}` };
+    // update metadata to clerk user
+    const client = await clerkClient();
+    client.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
+
+    // return { message: "Profile Created" };
+  } catch (error) {
+    return {
+      message: error instanceof Error ? error.message : "An error occurred",
+    };
   }
-};
+  redirect('/');
+}
