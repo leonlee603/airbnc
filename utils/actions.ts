5938397dde31c2154f5d78403cecb0ca092c1937
Leon <leepingho603@gmail.com>
2024-10-29 12:24:59 +0800

    Update - Add prisma schema, zod schema and action for property

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { imageSchema, profileSchema, validateWithZodSchema } from "@/utils/schemas";
+import { imageSchema, profileSchema, propertySchema, validateWithZodSchema } from "@/utils/schemas";
 import db from "./db";
 import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
@@ -141,3 +141,19 @@ export async function updateProfileImageAction (
     return renderError(error);
   }
 }
+
+export const createPropertyAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+
+  try {
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = validateWithZodSchema(propertySchema, rawData);
+    
+  } catch (error) {
+    return renderError(error);
+  }
+  redirect('/');
+};
--- utils/schemas.ts
+++ utils/schemas.ts
@@ -1,7 +1,7 @@
 import { z, ZodSchema } from "zod";
 
+// user profile schema
 export const profileSchema = z.object({
-  // firstName: z.string().max(5, { message: 'max length is 5' }),
   firstName: z.string().min(1, { message: 'First name is required' }),
   lastName: z.string().min(1, { message: 'Last name is required' }),
   username: z.string().min(1, { message: 'Username is required' }),
@@ -23,6 +23,7 @@ export function validateWithZodSchema<T>(
   return result.data;
 } 
 
+// image schema
 export const imageSchema = z.object({
   image: validateFile(),
 });
@@ -41,3 +42,50 @@ function validateFile() {
       );
     }, 'File must be an image');
 }
+
+// property schema
+export const propertySchema = z.object({
+  name: z
+    .string()
+    .min(2, {
+      message: 'Name is required and must be at least 2 characters.',
+    })
+    .max(100, {
+      message: 'Name must be less than 100 characters.',
+    }),
+  tagline: z
+    .string()
+    .min(2, {
+      message: 'Tagline is required and must be at least 2 characters.',
+    })
+    .max(100, {
+      message: 'Tagline must be less than 100 characters.',
+    }),
+  price: z.coerce.number().int().min(0, {
+    message: 'Price must be a positive number.',
+  }),
+  category: z.string(),
+  description: z.string().refine(
+    (description) => {
+      const wordCount = description.split(' ').length;
+      return wordCount >= 10 && wordCount <= 1000;
+    },
+    {
+      message: 'Description must be between 10 and 1000 words.',
+    }
+  ),
+  country: z.string(),
+  guests: z.coerce.number().int().min(0, {
+    message: 'Guest amount must be a positive number.',
+  }),
+  bedrooms: z.coerce.number().int().min(0, {
+    message: 'Bedrooms amount must be a positive number.',
+  }),
+  beds: z.coerce.number().int().min(0, {
+    message: 'Beds amount must be a positive number.',
+  }),
+  baths: z.coerce.number().int().min(0, {
+    message: 'Bahts amount must be a positive number.',
+  }),
+  amenities: z.string(),
+});
