244fe57b0727cffbdc0267cdebf4682ec71516fa
Leon <leepingho603@gmail.com>
2024-11-06 17:19:48 +0800

    Update - Show review function

--- utils/actions.ts
+++ utils/actions.ts
@@ -424,9 +424,34 @@ export async function deleteReviewAction(prevState: { reviewId: string }) {
       },
     });
 
-    revalidatePath('/reviews');
-    return { message: 'Review deleted successfully' };
+    revalidatePath("/reviews");
+    return { message: "Review deleted successfully" };
   } catch (error) {
     return renderError(error);
   }
+}
+
+/*
+ ** Review related
+ */
+
+export async function fetchPropertyRating(propertyId: string) {
+  const result = await db.review.groupBy({
+    by: ["propertyId"],
+    _avg: {
+      rating: true,
+    },
+    _count: {
+      rating: true,
+    },
+    where: {
+      propertyId,
+    },
+  });
+
+  // empty array if no reviews
+  return {
+    rating: result[0]?._avg.rating?.toFixed(1) ?? 0,
+    count: result[0]?._count.rating ?? 0,
+  };
 }
