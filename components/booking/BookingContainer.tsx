6f89061d0a87867cc14cafc9461b84d4d4128568
Leon <leepingho603@gmail.com>
2024-11-18 18:04:10 +0800

    Update - Add a few components and helper functions

--- components/booking/BookingContainer.tsx
+++ components/booking/BookingContainer.tsx
@@ -1,5 +1,21 @@
-export default function BookingContainer() {
+'use client';
+
+import { useProperty } from '@/utils/store';
+import ConfirmBooking from './ConfirmBooking';
+import BookingForm from './BookingForm';
+
+function BookingContainer() {
+  const { range } = useProperty((state) => state);
+
+  if (!range || !range.from || !range.to) return null;
+  if (range.to.getTime() === range.from.getTime()) return null;
+
   return (
-    <div>BookingContainer</div>
-  )
+    <div className='w-full'>
+      <BookingForm />
+      <ConfirmBooking />
+    </div>
+  );
 }
+
+export default BookingContainer;
